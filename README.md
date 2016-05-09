2c8d76a682a1f31933047c2fbce093330b0b2064
hyan <tristan_yan@intuit.com>
2016-05-09 10:50:53 -0700

    Repository initiative

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # golang
 Go language study
+
+1. Fundamental go language grammer.
+1-1. Const
+1-2. auto type detect.
+1-3. import
+1-4. loop and condition
---
+++ src/github.com/billyean/helloworld/helloworld.go
@@ -0,0 +1,17 @@
+package  main
+
+import (
+    "fmt"
+)
+
+const (
+  message = "你好，%s, %d!\n"
+  name = "严海波"
+  name2 = iota
+  name3
+)
+
+func main() {
+    b := 65
+    fmt.Printf("it's %c\n", b)
+}
